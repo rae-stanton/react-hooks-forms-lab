36eeb47a9f77973ca6fcf4b3845e73e4a546fa62
Rae Stanton <raestanton93@gmail.com>
2023-08-25 12:30:04 -0400

    fixing item form

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Filter({ onCategoryChange }) {
+  const [filterItems, setFilterItems] = useState('');
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input
+        type="text"
+        name="search"
+        placeholder="Search..."
+        value={filterItems}
+        onChange={(e) => setFilterItems(e.target.value)} // Added onChange event handler
+      />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
@@ -15,3 +23,8 @@ function Filter({ onCategoryChange }) {
 }
 
 export default Filter;
+
+//new input for searching
+// when user types, should filter items with matching names
+// make controlled value input
+// connct input to state - prop called onSearchChange
